3c0014b56f46725d56fa22bfc9b77dea16920b5c
Yannick Poffet <yannick@poffet.net>
2025-09-23 12:09:05 +0200

    Optimization of performances

--- CustomImageFilter.cpp
+++ CustomImageFilter.cpp
@@ -17,7 +17,7 @@ std::vector<int> sobelGy = {
 };
 
 
-void convolution(const ImageData& input, ImageData& output, std::vector<int> kernel) {
+void convolution(const ImageData& input, ImageData& output, const std::vector<int>& kernel) {
     output.setWidth(input.getWidth());
     output.setHeight(input.getHeight());
     output.setChannels(input.getChannels());
@@ -38,9 +38,6 @@ void convolution(const ImageData& input, ImageData& output, std::vector<int> ker
                     // Boundary check. If out of bounds, mirror edge pixels
                     if (posX < 0 || posX >= width) posX = x - kx;
                     if (posY < 0 || posY >= height) posY = y - ky;
-                    {
-                        /* code */
-                    }
 
                     int pixel = input.pixels[(posY) * width + (posX)];
 
@@ -52,7 +49,8 @@ void convolution(const ImageData& input, ImageData& output, std::vector<int> ker
             convol_res = std::abs(convol_res);
             // Clamp the result to [0, 255]
             convol_res = std::clamp(convol_res, 0, 255);
-            output.setPixel(x, y, static_cast<unsigned char>(convol_res));
+
+            output.pixels[y * width + x] = static_cast<unsigned char>(convol_res);
         }
     }
 }
@@ -124,18 +122,19 @@ std::vector<unsigned int> CustomImageFilter::computeMinimalEnergyPathMap(ImageDa
     // Fill in the cumulative energy map
     for (int y = 1; y < energyMap.getHeight(); ++y) {
         for (int x = 0; x < energyMap.getWidth(); ++x) {
-
             unsigned int idx = y * energyMap.getWidth() + x;
             unsigned int above = (y - 1) * energyMap.getWidth() + x;
 
-            std::vector<unsigned int> candidates = {};
-            candidates.push_back(minimalEnergyPathMap[above]); // directly above
-
-            if((x - 1) >= 0) candidates.push_back(minimalEnergyPathMap[above - 1]);
-            if((x + 1) < energyMap.getWidth()) candidates.push_back(minimalEnergyPathMap[above + 1]);
-
-            // Find the minimum cumulative energy from the three possible pixels above
-            unsigned int minEnergy = *std::min_element(candidates.begin(), candidates.end());
+            // Directly above
+            unsigned int minEnergy = minimalEnergyPathMap[above];
+            // Above-left
+            if (x - 1 >= 0) {
+                minEnergy = std::min(minEnergy, minimalEnergyPathMap[above - 1]);
+            }
+            // Above-right
+            if (x + 1 < static_cast<int>(energyMap.getWidth())) {
+                minEnergy = std::min(minEnergy, minimalEnergyPathMap[above + 1]);
+            }
 
             // Update the cumulative energy for the current pixel
             minimalEnergyPathMap[idx] = static_cast<unsigned int>(energyMap.pixels[idx]) + minEnergy;
@@ -153,7 +152,6 @@ std::vector<unsigned int> CustomImageFilter::identityMinEnergySeam(const std::ve
 
     // Find the starting point of the minimal energy seam in the last row
     auto lastRowStart = minPathEnergyMap.end() - imageWidth;
-    printf("Last row energies: %u\n", *lastRowStart);
     auto minIt = std::min_element(lastRowStart, minPathEnergyMap.end());
     int seamPosX = std::distance(lastRowStart, minIt);
 
